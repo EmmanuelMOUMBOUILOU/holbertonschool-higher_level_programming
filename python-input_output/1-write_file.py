c71839054d56f060706be7e8354549e3b4af08d5
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-06-03 15:41:28 +0200

    Updated documentation for the write_file function to clarify the description of its behavior.

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,9 @@ This module provides a function to write a string to a UTF-8 text file.
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a UTF-8 text file and returns the number of characters written."""
-    with open(filename, "w", encoding="utf-8") as f:
+    """
+    Writes a string to a text file
+    and returns the number of characters written.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
