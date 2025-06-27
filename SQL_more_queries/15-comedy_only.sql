12ad51d1a8797f88168ae7c89b7d6c5af6e997d2
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-06-27 19:29:44 +0200

    Correction of column identifier in join for SQL script to select comedy series

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
--- File: SQL_more_queries/14-my_genres.sql
+-- File: SQL_more_queries/15-only_comedy.sql
 SELECT tv_shows.title
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
