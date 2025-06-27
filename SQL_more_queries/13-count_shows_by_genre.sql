2be5d2305700e540872424d58a35e72c6970a940
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-06-27 17:51:01 +0200

    Add a SQL script to count TV shows by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- File: SQL_more_queries/13-count_shows_by_genre.sql
+SELECT tv_genres.name AS genre,
+       COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+HAVING COUNT(tv_show_genres.tv_show_id) > 0
+ORDER BY number_of_shows DESC;
