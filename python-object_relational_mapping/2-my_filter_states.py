4d2f00ef560f4bf2d0bfb881f254e90aac80c0c2
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-06-27 21:18:28 +0200

    Add a script to display the values in the status table corresponding to a given name in the hbtn_0e_0_usa database.

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Displays all values in the states table where name matches the argument
+from the database hbtn_0e_0_usa. Results are sorted by states.id ascending."""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """Connects to MySQL and fetches states matching the given name."""
+    username, password, db_name, state_name = sys.argv[1:5]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+
+    query = (
+        "SELECT * FROM states "
+        "WHERE name = '{}' "
+        "ORDER BY id ASC"
+    ).format(state_name)
+
+    cursor.execute(query)
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
