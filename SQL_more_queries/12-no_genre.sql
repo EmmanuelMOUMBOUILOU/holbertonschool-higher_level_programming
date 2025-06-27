c1c5f5b735f28fa7c1992bdd6f2170b5e73ece27
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-06-27 20:05:02 +0200

    Added genre identifier to TV series selection and adjusted sort order

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- File: SQL_more_queries/12-no_genre.sql
-SELECT tv_shows.title
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
