2ffc80276660e9cef0b38a70f1880c6e1825f451
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-06-27 21:00:50 +0200

    Refactor code structure by removing the main function and simplifying the execution flow in 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""List all states from the database hbtn_0e_0_usa"""
-
 import MySQLdb
 import sys
 
-
-def main():
-
+if __name__ == "__main__":
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=db_name)
-
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
 
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+    for row in rows:
+        print(row)
 
     cursor.close()
     db.close()
-
-if __name__ == "__main__":
-    main()
