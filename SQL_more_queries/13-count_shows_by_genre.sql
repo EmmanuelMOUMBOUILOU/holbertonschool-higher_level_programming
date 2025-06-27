33efd6c75a660e40c2ae6a13d555c8537dab9be6
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-06-27 19:29:10 +0200

    Correction of SQL query to use LEFT JOIN and adjustment of GROUP BY for counting emissions by gender

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,6 @@
 -- File: SQL_more_queries/13-count_shows_by_genre.sql
-SELECT tv_genres.name AS genre,
-       COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
-HAVING COUNT(tv_show_genres.tv_show_id) > 0
-ORDER BY number_of_shows DESC;
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
+ORDER BY number_of_shows DESC, genre ASC;
