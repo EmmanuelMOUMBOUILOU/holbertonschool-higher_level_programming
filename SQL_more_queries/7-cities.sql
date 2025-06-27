23fd3f7d3ebdb09f15fa3dc3fdce42eda4f0f073
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-06-27 17:47:06 +0200

    Add a SQL script to create the cities table in the hbtn_0d_usa database

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- File: SQL_more_queries/7-cities.sql
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
